1374b40ef22d2c6ee26c4eca525aa38014e386dd
Domestos002 <ENbLEiuPm74p2946333314>
2016-10-05 17:50:26 +0600

    do some blocks

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,1 +1,8 @@
-console.log('hi2')
+$(document).ready(function () {
+
+    if ($('input.data-inputmask, .inputmask-phone').length > 0) {
+        $('input.data-inputmask, .inputmask-phone').mask("+7 (999) 999-99-99");
+    }
+
+});
+

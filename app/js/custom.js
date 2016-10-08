63a02c3ead9757144667845f7d218f063ead184e
Domestos002 <ENbLEiuPm74p2946333314>
2016-10-08 16:45:41 +0600

    do a responsive

--- app/js/custom.js
+++ app/js/custom.js
@@ -4,5 +4,32 @@ $(document).ready(function () {
         $('input.data-inputmask, .inputmask-phone').mask("+7 (999) 999-99-99");
     }
 
+    /*$(document).on("click", '.burger', function(e) {
+        $(".header-nav-mobile").toggleClass('hidden');
+        e.preventDefault();
+    });*/
+
+    $(document).on("click", '.burger', function(e) {
+        $('.sliding-panel').addClass('active');
+        $('.burger').addClass('active');
+        $('body').addClass('ovh');
+        e.preventDefault();
+    });
+
+    $(document).on("click", '.sliding-panel__close', function(e) {
+        $('.sliding-panel').removeClass('active');
+        $('.burger').removeClass('active');
+        $('body').removeClass('ovh');
+        e.preventDefault();
+    });
+
+    /*$(document).on("click", '.burger', function(e) {
+        $(this).closest(".sliding-panel-item").toggleClass('active');
+        $(this).closest(".sliding-panel-item").find('.sliding-panel-dd').stop().slideToggle(400);
+        $(this).closest(".sliding-panel-link").toggleClass('active');
+        e.preventDefault();
+    });
+*/
+
 });
 

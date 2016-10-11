cad0a7de6b77ec8c5296b0db3434a9acbc68143a
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2016-10-11 14:25:00 +0600

    minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -152,12 +152,19 @@ gulp.task('clean', function(){
 /* BUILD -------------------------------------------------------------------- */
 gulp.task('build',["clean"], function(){
     setTimeout(function () {
-        return gulp.src(sources.html.src)
+        gulp.src(sources.html.src)
             .pipe(useref())
             .pipe(gulpif('*.js', uglify()))
             .pipe(gulpif('*.css', minifyCss()))
             .pipe(useref())
             .pipe(gulp.dest('dist'));
+
+        gulp.src("app/fonts/**/*")
+            .pipe(gulp.dest('dist/fonts'));
+
+        gulp.src("app/images/**/*")
+            .pipe(gulp.dest('dist/images'));
+
     }, 500);
 });
 
